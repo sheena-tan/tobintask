9d3a46f51bd1fa204effed8d7ce49e6d97fcab88
Sheena Tan <sheenatan2024@u.northwestern.edu>
2024-04-23 16:28:22 -0500

    reported exercise 1 and 2 results

---
+++ task1/scripts/2_eda.R
@@ -0,0 +1,19 @@
+# exercise 3 (data visualization)
+
+library(tidyverse)
+library(here)
+
+# load data
+load(here("task1/data/mma_data.rds"))
+
+# a) Describe how the distribution of Saidin Index scores varies across the sample of
+# hospitals. 1 of 3
+# b) Looking at the Saidin Index scores you’ve calculated, do different types of hospitals
+# have different rates of technology adoption?
+#   c) Do hospitals with more beds tend to have higher Saidin index scores?
+#   Output: Provide written answers to each question, with supporting evidence, in your
+# write up. (Also make sure to include the code used to produce your results in your .do
+#            file.)
+
+# looking at 2004 only
+# output to include explanations of graph/table(s)
