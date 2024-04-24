8e9e1bf5093d8664774e88f7e6af9b862ef4db2c
Sheena Tan <sheenatan2024@u.northwestern.edu>
2024-04-23 19:33:55 -0500

    report for exercise 3a

--- task1/scripts/2_eda.R
+++ task1/scripts/2_eda.R
@@ -9,12 +9,40 @@ library(viridis)
 # load data
 load(here("task1/data/mma_data.rds"))
 
-# Distribution of Saidin Index scores by year ----
 mma_data <- mma_data |>
   mutate(year = as.factor(year)) |>
   mutate(year = fct_rev(year))
 
-# density ridges
+###############################################################################
+# EXERCISE 3a
+###############################################################################
+
+# Summary statistics ----
+mma_2004 <- mma_data |>
+  filter(year == 2004)
+
+describe(mma_2004$saidin, IQR = TRUE, skew = FALSE)
+
+# Visualize distribution ----
+p1 <- ggplot(mma_2004, aes(saidin)) +
+  geom_boxplot() +
+  xlim(0, 12) +
+  theme_void()
+
+p2 <- ggplot(mma_2004, aes(saidin)) +
+  geom_density() +
+  xlim(0, 12) +
+  theme_minimal() +
+  theme(
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank()
+  ) +
+  labs(x = "Saidin Index Scores")
+
+saidin_2004 <- p1/p2 + plot_layout(heights = unit(c(1, 5), c("cm", "null")))
+
+# Distribution of Saidin Index scores by year ----
+# density ridges plot
 saidin_density_ridge <- mma_data |>
   ggplot(aes(x = saidin, y = year)) +
   geom_density_ridges(aes(fill = year, color = year), scale = 3) +
@@ -63,18 +91,27 @@ saidin_boxplot <- mma_data |>
   )
 
 # write out plots
+ggsave(here("task1/figures/saidin_2004.png"), saidin_2004,
+       width = 7.8, height = 6.0, units = "in")
+
 ggsave(here("task1/figures/saidin_density_ridge.png"), saidin_density_ridge,
        width = 7.8, height = 6.4, units = "in")
 
 ggsave(here("task1/figures/saidin_boxplot.png"), saidin_boxplot,
        width = 7.8, height = 6.4, units = "in")
 
+
+###############################################################################
+# EXERCISE 3b
+###############################################################################
+
 # b) Looking at the Saidin Index scores you’ve calculated, do different types of hospitals
 # have different rates of technology adoption?
-#   c) Do hospitals with more beds tend to have higher Saidin index scores?
-#   Output: Provide written answers to each question, with supporting evidence, in your
-# write up. (Also make sure to include the code used to produce your results in your .do
-#            file.)
 
-# looking at 2004 only
-# output to include explanations of graph/table(s)
+
+###############################################################################
+# EXERCISE 3c
+###############################################################################
+
+#   c) Do hospitals with more beds tend to have higher Saidin index scores?
+
